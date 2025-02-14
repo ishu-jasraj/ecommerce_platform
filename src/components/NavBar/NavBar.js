fb38f5b4df4b80d6ab5dca4f783c412907cd5361
ishu-jasrajpuria <ishu.jasrajpuria@antwalk.com>
2025-02-14 16:33:12 +0530

    show cart length in navigation bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './NavBar.css';
+import { CartContext } from '../../store/cart-context';
 
 const NavBar = () => {
+    const { cart } = useContext(CartContext);
+    const total =  cart.reduce((total, item)=>{
+        return total + item.quantity;
+    },0);
+
     const navigate = useNavigate();
     return(
         <nav>
             <button onClick={()=>navigate('/')}>Products</button>
-            <button onClick={()=>navigate('/cart')}>Cart</button>
+            <button onClick={()=>navigate('/cart')}>Cart ({total})</button>
         </nav>
     )
 }
