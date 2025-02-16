6451ebb774ebf61e2dc97151ba3543c618b73bde
ishu-jasrajpuria <ishu.jasrajpuria@antwalk.com>
2025-02-16 20:13:27 +0530

    implement logout feature

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './NavBar.css';
 import { CartContext } from '../../store/cart-context';
 import { UserAuthContext } from '../../store/user-auth-context';
 
 const NavBar = () => {
-    const { isAuthenticated, username } = useContext(UserAuthContext);
+    const { isAuthenticated, setIsAuthenticated, username } = useContext(UserAuthContext);
     const { cart } = useContext(CartContext);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
     const total = cart.reduce((total, item) => total + item.quantity, 0);
 
     const navigate = useNavigate();
+
+    const onLogout = () => {
+        setDropdownOpen(false);
+        setIsAuthenticated(false);
+        navigate('/login');
+    }
+
     return (
         <nav>
             {/* Center: Products & Cart with 10px gap */}
@@ -21,7 +30,20 @@ const NavBar = () => {
             {/* Right side: Login/Signup OR Username */}
             <div className="nav-right">
                 {isAuthenticated ? (
-                    <span className="nav-username">Hey, {username} 👋</span>
+                    <div className="dropdown nav-username">
+                    <span>Hey, {username} 👋</span>
+                    <span 
+                    className={`dropdown-arrow ${dropdownOpen ? 'open' : ''}`}
+                    onClick={()=>setDropdownOpen(!dropdownOpen)}
+                    > ▼ </span>
+                    {dropdownOpen && (
+                        <div className="dropdown-menu">
+                            <button>profile</button>
+                            <button>set theme</button>
+                            <button onClick={onLogout}>logout</button>
+                        </div>
+                    )}
+                    </div>
                 ) : (
                     <>
                         <button onClick={() => navigate('/login')}>Login</button>
